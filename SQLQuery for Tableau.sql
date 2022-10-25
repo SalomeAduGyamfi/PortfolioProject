6812f2098ac5d916265cfbce79f7afe03b5e2fec
Salome Adu-Gyamfi <116242322+SalomeAduGyamfi@users.noreply.github.com>
2022-10-25 15:56:03 -0400

    Add files via upload

---
+++ SQLQuery for Tableau.sql
@@ -0,0 +1,62 @@
+/*
+Queries used for Tableau Project
+*/
+
+-- GLOBAL Total Numbers
+
+Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
+From PortfolioProject..CovidDeaths
+-- Where location like '%states%'
+where continent is not null 
+-- Group By date
+order by 1,2
+
+-- Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
+-- From PortfolioProject..CovidDeaths
+-- Where location like '%states%'
+-- where location = 'World'
+-- Group By date
+-- order by 1,2
+
+-- We take these out as they are not inluded in the above queries and want to stay consistent
+-- European Union is part of Europe
+
+
+Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
+From PortfolioProject..CovidDeaths
+--Where location like '%states%'
+Where continent is null 
+and location not in ('World', 'European Union', 'International')
+Group by location
+order by TotalDeathCount desc
+
+Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From PortfolioProject..CovidDeaths
+--Where location like '%states%'
+Group by Location, Population
+order by PercentPopulationInfected desc
+
+Select Location, Population,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From PortfolioProject..CovidDeaths
+--Where location like '%states%'
+Group by Location, Population, date
+order by PercentPopulationInfected desc
+
+-- Total death vs Vaccinations
+-- Shows Percentage of Total death that has recieved at least one Covid Vaccine
+
+Select dea.continent, dea.location, dea.date, dea.total_deaths, vac.new_vaccinations
+, SUM(CONVERT(int,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
+--, (RollingPeopleVaccinated/total_death)*100
+From PortfolioProject..CovidDeaths dea
+Join PortfolioProject..CovidVaccinations vac
+	On dea.location = vac.location
+	and dea.date = vac.date
+where dea.continent is not null 
+order by 2,3
+
+
+
+
+
+
